95dbca424fe3cf5ed79fa5e750215560daa06364
amos-kibet <kibetamos97@gmail.com>
2023-01-03 19:58:08 +0300

    add live_box schema file

---
+++ lib/tool_box/live_box/live_box_item.ex
@@ -0,0 +1,19 @@
+defmodule ToolBox.LiveBox.LiveBoxItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "box_items" do
+    field :short_description, :string
+    field :snapshot, :string
+    field :url, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(live_box_item, attrs) do
+    live_box_item
+    |> cast(attrs, [:short_description, :snapshot, :url])
+    |> validate_required([:short_description, :snapshot, :url])
+  end
+end
